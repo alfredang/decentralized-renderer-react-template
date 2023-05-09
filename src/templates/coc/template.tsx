cf3605e2cc68ec66878ecabf12f4bc77fde2b6a1
jinng1 <72145074+jinng1@users.noreply.github.com>
2023-05-09 17:02:28 +0800

    updated template

--- src/templates/coc/template.tsx
+++ src/templates/coc/template.tsx
@@ -1,19 +1,86 @@
 import React, { FunctionComponent } from "react";
 import { TemplateProps } from "@govtechsg/decentralized-renderer-react-components";
 import { CocTemplateCertificate } from "../samples/customTemplateSample";
+import BackgroundImage from "../static/CertificateBlank.jpg";
 
 export const CocTemplate: FunctionComponent<TemplateProps<CocTemplateCertificate> & { className?: string }> = ({
   document,
   className = ""
 }) => {
   return (
-    <div className={className} id="custom-template">
-      <h1>{document.name}</h1>
-      <div>This certifies that</div>
-      <h2>{document.recipient.name}</h2>
-      <div>has successfully completed</div>
-      <h2>{document.recipient.courseName}</h2>
-      <h2> Held on {document.recipient.courseDates}</h2>
+    <div
+      style={{
+        width: "800px",
+        height: "600px",
+        backgroundImage: `url(${BackgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        textAlign: "center",
+        fontFamily: "Arial, sans-serif",
+        position: "relative" as const
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: "43%",
+          left: 0,
+          width: "100%",
+          fontSize: "24px",
+          fontFamily: "Quintessential"
+        }}
+      >
+        This certifies that
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: 0,
+          width: "100%",
+          fontSize: "24px",
+          fontFamily: "Quattrocento"
+        }}
+      >
+        {document.recipient.name}
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          top: "55%",
+          left: 0,
+          width: "100%",
+          fontSize: "24px",
+          fontFamily: "Quintessential"
+        }}
+      >
+        has successfully completed the
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          top: "62%",
+          left: 0,
+          width: "100%",
+          fontSize: "34px",
+          fontWeight: "bold",
+          fontFamily: "Quattrocento"
+        }}
+      >
+        {document.recipient.courseName}
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          top: "71%",
+          left: 0,
+          width: "100%",
+          fontSize: "22px",
+          fontFamily: "Quattrocento"
+        }}
+      >
+        Held on {document.recipient.courseDates}
+      </div>
     </div>
   );
 };
